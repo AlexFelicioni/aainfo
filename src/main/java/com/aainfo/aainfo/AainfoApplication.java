04ce1754587aaf80109e10755f04b2de020aae77
AlexFelicioni <alexfelicioni@gmail.com>
2023-05-13 16:29:51 -0300

    projetc created

---
+++ src/main/java/com/aainfo/aainfo/AainfoApplication.java
@@ -0,0 +1,13 @@
+package com.aainfo.aainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AainfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AainfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aainfo/aainfo/AainfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aainfo.aainfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AainfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
