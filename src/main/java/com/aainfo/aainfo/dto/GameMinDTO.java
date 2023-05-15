bbfd36c5466220386af380a9a103e65704ffb62e
AlexFelicioni <alexfelicioni@gmail.com>
2023-05-15 11:13:18 -0300

    /list/id/game, gameProjection

--- src/main/java/com/aainfo/aainfo/dto/GameMinDTO.java
+++ src/main/java/com/aainfo/aainfo/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.aainfo.aainfo.dto;
 
 import com.aainfo.aainfo.entities.Game;
+import com.aainfo.aainfo.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
     	year=entity.getYear();
     	imgUrl=entity.getImgUrl();
     	shortDescription=entity.getShortDescription();
+     	
+    }
+    public GameMinDTO(GameMinProjection projection) {
+    	id=projection.getId();
+    	title=projection.getTitle();
+    	year=projection.getYear();
+    	imgUrl=projection.getImgUrl();
+    	shortDescription=projection.getShortDescription();
      	
     }
 
